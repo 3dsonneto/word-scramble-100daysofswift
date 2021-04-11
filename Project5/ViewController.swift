c86d04703aa4306b7b682f6e63ea4e84ab5b619f
Edson <edson_neto1996@hotmail.com>
2021-04-11 04:00:20 -0300

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Edson Neto on 11/04/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //procura o arquivo start.txt no bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsURL){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+
+    }
+
+
+}
+
